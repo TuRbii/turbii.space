bf1107545621c6434001651fc42550a6a131f976
TuRbii <tschelevitz@gmail.com>
2023-04-29 23:11:30 -0400

    added gradient background and confetti

--- turbii.space/main.js
+++ turbii.space/main.js
@@ -31,3 +31,19 @@ function nextImage(direction) {
   }
   images[index].classList.add("main");
 }
+
+const start = () => {
+  setTimeout(function() {
+      confetti.start()
+  }, 1000); 
+};
+
+const stop = () => {
+  setTimeout(function() {
+      confetti.stop()
+  }, 5000); 
+};
+
+start();
+stop();
+
